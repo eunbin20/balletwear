89bbf764c5a4a25e7032269345a06b4f01075a3e
eunbin20 <eunbines2@gmail.com>
2024-03-14 23:57:04 +0900

    feature: 첫 번째로 선택하는 색상 정보 가져오기

--- app/crawling/leotards/route.ts
+++ app/crawling/leotards/route.ts
@@ -1,9 +1,12 @@
 import { NextResponse } from "next/server";
 const axios = require("axios");
 const cheerio = require("cheerio");
-const { Builder, By, Key, until } = require("selenium-webdriver");
-const chrome = require("selenium-webdriver/chrome");
+
 export const dynamic = "force-dynamic";
+
+// selenium
+const { Builder, By } = require("selenium-webdriver");
+const chrome = require("selenium-webdriver/chrome");
 
 // GET http://localhost:3000/crawling/leotards
 // 유미코 커스텀 오더 페이지의 html 파일을 가져온다.
@@ -11,26 +14,70 @@ export async function GET(request: Request) {
   const leotards = await getLeotardList();
   const colors = await getColorList();
 
-  return NextResponse.json({ leotards, color: {} });
+  return NextResponse.json({ leotards, color: colors });
 }
 
 const getColorList = async () => {
-  console.log("getColorList");
-  const { data } = await getHtml("https://mto.yumiko.com/us_en/alex.html");
-  const query = cheerio.load(data);
-  const ColorListQuery = query(".fabric");
-  let colors: string[] = [];
-  ColorListQuery.each((_: any, node: any) => {
-    console.log(node);
-    const leotardName = query(node)
-      .find(".product-item-link")
-      .text()
-      .replace(/\s+|\n/g, "");
+  // headless로 크롬 드라이버 실행
+  const driver = await new Builder()
+    .forBrowser("chrome")
+    .setChromeOptions(
+      new chrome.Options().windowSize({ width: 1920, height: 1080 })
+    )
+    .setChromeService(
+      new chrome.ServiceBuilder("/Users/eunbin.kim/Downloads/chromedriver")
+    )
+    .build();
 
-    colors.push(leotardName);
-  });
+  try {
+    await driver.get("https://mto.yumiko.com/us_en/alex.html");
 
-  return colors;
+    // 컬러 옵션 모달을 여는 버튼을 클릭한다.
+    const colorOptionButton = await driver.findElement(
+      By.className("color-option-modal-trigger")
+    );
+    colorOptionButton.click();
+
+    await driver.findElement(By.className("page-wrapper"));
+    const fabricHeadings = await driver.findElements(
+      By.className("fabric-heading")
+    );
+    const fabricContainers = await driver.findElements(
+      By.className("fabric-container")
+    );
+    const result: {
+      [key: string]: {
+        name: string;
+        sampleImage: string;
+      }[];
+    } = {};
+
+    for (let i = 0; i < fabricHeadings.length; i++) {
+      const fabricHeading = await fabricHeadings[i].getText();
+      const fabrics = [];
+
+      for (let i = 0; i < fabricContainers.length; i++) {
+        const fabricName = await fabricContainers[i]
+          .findElement(By.className("fabric-name"))
+          .getText();
+        const fabricImage = await fabricContainers[i]
+          .findElement(By.tagName("img"))
+          .getAttribute("src");
+
+        fabrics.push({
+          name: fabricName,
+          sampleImage: fabricImage,
+        });
+      }
+
+      result[fabricHeading] = fabrics;
+    }
+    return result;
+  } catch (error) {
+    console.log(error);
+  } finally {
+    driver.quit();
+  }
 };
 
 const getLeotardList = async () => {
@@ -39,7 +86,7 @@ const getLeotardList = async () => {
   );
   const query = cheerio.load(data);
   const leotardListQuery = query(".product-item");
-  let leotards: string[] = [];
+  const leotards: string[] = [];
   leotardListQuery.each((_: any, node: any) => {
     const leotardName = query(node)
       .find(".product-item-link")
@@ -56,6 +103,6 @@ const getHtml = async (url: string) => {
   try {
     return await axios.get(url);
   } catch (error) {
-    console.log("error", error);
+    console.log(error);
   }
 };
