e04bca9820a5916949ecdc9514c1293bc61ea3c5
Eunbin Kim <43979066+eunbin20@users.noreply.github.com>
2024-11-07 23:51:28 +0900

    feature: 반응형 스타일 대응 (#2)

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 
 export function Header() {
   return (
-    <header className="w-full h-12 p-2 ml-2 border-b-[1px] border-gray-300 shadow-sm z-10 bg-white">
+    <header className="w-full h-12 p-2 border-b-[1px] border-gray-300 shadow-sm z-10 bg-white">
       <div className="font-semibold text-lg ">Yumico Simulator</div>
     </header>
   );
